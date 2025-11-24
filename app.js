9844b90612d62f24371bc5f96958c32b78d7d473
Ashish Singh <ashish808116@gmail.com>
2025-11-25 01:45:28 +0530

    initial commit

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbURL = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const dbURL = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -34,7 +34,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(dbURL);
+  await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
@@ -45,7 +45,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-  mongoUrl: dbURL,
+  mongoUrl: MONGO_URL,
   crypto: {
     secret: process.env.SECRET,
   },
